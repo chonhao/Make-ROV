b8d7422fa232904cea7704aa3b8992e276e37bfb
chonhouleong <lch27thomas@gmail.com>
2015-04-15 20:04:33 +0800

    Bluetooth added
    bluetooth works, receive data successfully, but not able to add data to
    chart.

--- Make-ROV-android/app/src/main/java/com/rov/pcms/make_rov/SensorMoitor.java
+++ Make-ROV-android/app/src/main/java/com/rov/pcms/make_rov/SensorMoitor.java
@@ -1,11 +1,12 @@
 package com.rov.pcms.make_rov;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
-import android.graphics.Color;
-import android.graphics.Typeface;
+import android.hardware.Sensor;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -23,16 +24,16 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.components.YAxis;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.LineData;
-import com.github.mikephil.charting.data.LineDataSet;
-import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
-import com.github.mikephil.charting.utils.ColorTemplate;
-import com.github.mikephil.charting.utils.Highlight;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import javax.net.ssl.SSLEngine;
 
 
 public class SensorMoitor extends ActionBarActivity {
@@ -42,46 +43,65 @@ public class SensorMoitor extends ActionBarActivity {
     private ListView drawerList;
     private android.support.v7.app.ActionBarDrawerToggle drawerListener;
     MyAdapter myAdapter;
-    private static ChartGraph sensor1chart = new ChartGraph();
-    private static ChartGraph sensor2chart = new ChartGraph();
-    private static ChartGraph sensor3chart = new ChartGraph();
+    private static ChartGraph[] sensorChart = {
+            new ChartGraph(),new ChartGraph(),
+            new ChartGraph(),new ChartGraph(),
+    };
 
 
     //------------shared prefrence-------------
     public SharedPreferences sharedPreferences;
 
+    //------------Bluetooth Components-----------------------------------
+    BluetoothAdapter mBluetoothAdapter;
+    BluetoothDevice lastDevice;
+    BluetoothThread thread;
+    String[] itemString;
+    List<String> itemList = new ArrayList<>();
+    private Set<BluetoothDevice> paired;
+
+    //------------Packet Components--------------------------------------
+    private InputStream inStream;
+    private static final Character SENSOR_HEADER = '*';
+    private static final Character SENSOR_SERIAL_TYPE = '6';
+    private static int currentReadMode = 0;
+    private static int currentCount = 1;
+    private static int tempValue = 0;
+    private static int[] sensorValue = new int[5];
+    boolean stopWorker = false;
+    private int selected = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sensor_moitor);
 //-----------------UI init--------------------------------------
-        Toolbar toolbar = (Toolbar)findViewById(R.id.app_bar2);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar2);
         setSupportActionBar(toolbar);
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         setTitle("Sensor Monitor");
 //-------------check if it is first run--------------------------
-        sharedPreferences = getSharedPreferences(BasicInformationActivity.ROV_BASIC_INFORMATION,MODE_PRIVATE);
-        if(!sharedPreferences.getString(BasicInformationActivity.FIRST_TIME_SETUP,"true").equals("false")) //debug
-            startActivity(new Intent(SensorMoitor.this,setup_BasicInformationActivity.class));
+        sharedPreferences = getSharedPreferences(BasicInformationActivity.ROV_BASIC_INFORMATION, MODE_PRIVATE);
+        if (!sharedPreferences.getString(BasicInformationActivity.FIRST_TIME_SETUP, "true").equals("false")) //debug
+            startActivity(new Intent(SensorMoitor.this, setup_BasicInformationActivity.class));
 //-------------------INIT Nav Drawer---------------------------
-        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
-        drawerList = (ListView)findViewById(R.id.left_drawer);
-        myAdapter=new MyAdapter(this);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerList = (ListView) findViewById(R.id.left_drawer);
+        myAdapter = new MyAdapter(this);
         drawerList.setAdapter(myAdapter);
         LayoutInflater inflater = getLayoutInflater();
         //add a picture in the Nav drawer as header
-        ViewGroup header = (ViewGroup) inflater.inflate(R.layout.headerview, drawerList,false);
-        drawerList.addHeaderView(header,null,false);
+        ViewGroup header = (ViewGroup) inflater.inflate(R.layout.headerview, drawerList, false);
+        drawerList.addHeaderView(header, null, false);
         drawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 //                Toast.makeText(BasicInformationActivity.this, Long.toString(id), Toast.LENGTH_SHORT).show();
-                switch ((int)id) {
+                switch ((int) id) {
                     case 1:
-                        startActivity(new Intent(SensorMoitor.this,BasicInformationActivity.class));
+                        startActivity(new Intent(SensorMoitor.this, BasicInformationActivity.class));
                         break;
                     case 2:
                         startActivity(new Intent(SensorMoitor.this, MultiMotorAllocationActivity.class));
@@ -93,7 +113,7 @@ public class SensorMoitor extends ActionBarActivity {
                 drawerLayout.closeDrawer(drawerList);
             }
         });
-        drawerListener = new android.support.v7.app.ActionBarDrawerToggle(this,drawerLayout,null,0,0){
+        drawerListener = new android.support.v7.app.ActionBarDrawerToggle(this, drawerLayout, null, 0, 0) {
             @Override
             public void onDrawerClosed(View drawerView) {
                 super.onDrawerClosed(drawerView);
@@ -107,16 +127,16 @@ public class SensorMoitor extends ActionBarActivity {
         };
         drawerLayout.setDrawerListener(drawerListener);
 //---------------custom action starts-----------------------------
-        sensor1chart.lineChart = (LineChart)findViewById(R.id.sensor1chart);
-        sensor1chart.chartInit();
+        sensorChart[1].lineChart = (LineChart) findViewById(R.id.sensor1chart);
+        sensorChart[1].chartInit();
 
-        sensor2chart.lineChart = (LineChart)findViewById(R.id.sensor2chart);
-        sensor2chart.chartInit();
-        sensor3chart.lineChart = (LineChart)findViewById(R.id.sensor3chart);
-        sensor3chart.chartInit();
+        sensorChart[2].lineChart = (LineChart) findViewById(R.id.sensor2chart);
+        sensorChart[2].chartInit();
 
-
-
+        sensorChart[3].lineChart = (LineChart) findViewById(R.id.sensor3chart);
+        sensorChart[3].chartInit();
+//-------------bluetooth--------------------------------------
+        Bluetoothinit();
 
     }
 
@@ -140,14 +160,14 @@ public class SensorMoitor extends ActionBarActivity {
     public boolean onKeyUp(int keyCode, KeyEvent event) {
         if (keyCode == KeyEvent.KEYCODE_MENU) {
             // ........
-            for(int i =0;i<=20;i++) {
-                sensor1chart.addEntry((float) (Math.random() * 10));
-                sensor2chart.addEntry((float) (Math.random() * 10));
-                sensor3chart.addEntry((float) (Math.random() * 10));
+            for (int i = 0; i <= 20; i++) {
+                sensorChart[1].addEntry((float) (Math.random() * 10));
+                sensorChart[2].addEntry((float) (Math.random() * 10));
+                sensorChart[3].addEntry((float) (Math.random() * 10));
             }
-            sensor1chart.lineChart.animateX(1000);
-            sensor2chart.lineChart.animateX(1000);
-            sensor3chart.lineChart.animateX(1000);
+            sensorChart[1].lineChart.animateX(1000);
+            sensorChart[2].lineChart.animateX(1000);
+            sensorChart[3].lineChart.animateX(1000);
             return true;
         }
         return super.onKeyUp(keyCode, event);
@@ -165,17 +185,24 @@ public class SensorMoitor extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        sensor1chart.addEntry((float)(Math.random() * 10));
-        sensor2chart.addEntry((float)(Math.random() * 10));
-        sensor3chart.addEntry((float)(Math.random() * 10));
 
         int id = item.getItemId();
+            // ........
+//            for (int i = 0; i <= 20; i++) {
+//                sensorChart[1].addEntry((float) (Math.random() * 10));
+//                sensorChart[2].addEntry((float) (Math.random() * 10));
+//                sensorChart[3].addEntry((float) (Math.random() * 10));
+//            }
+//            sensorChart[1].lineChart.animateX(1000);
+//            sensorChart[2].lineChart.animateX(1000);
+//            sensorChart[3].lineChart.animateX(1000);
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_scan) {
+            requestEnableBluetooth();
             return true;
         }
-        if(drawerListener.onOptionsItemSelected(item)){
+        if (drawerListener.onOptionsItemSelected(item)) {
             return true;
         }
 
@@ -191,7 +218,8 @@ public class SensorMoitor extends ActionBarActivity {
     class MyAdapter extends BaseAdapter {
         private Context context;
         String[] features;
-        public MyAdapter(Context context){
+
+        public MyAdapter(Context context) {
             this.context = context;
             features = context.getResources().getStringArray(R.array.features_array);
         }
@@ -214,11 +242,11 @@ public class SensorMoitor extends ActionBarActivity {
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
             View row = null;
-            if(convertView == null){
+            if (convertView == null) {
                 LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                row = inflater.inflate(R.layout.custom_nav_drawer,parent,false);
+                row = inflater.inflate(R.layout.custom_nav_drawer, parent, false);
 
-            }else{
+            } else {
                 row = convertView;
             }
             TextView motorTextField = (TextView) row.findViewById(R.id.motorTextView);
@@ -227,8 +255,149 @@ public class SensorMoitor extends ActionBarActivity {
         }
     }
 
-    //debug
+    //Bluetooth Components
+
+    private void Bluetoothinit() {
+    mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    if (mBluetoothAdapter == null) //no bluetooth support
+        return;
+
+    if (!mBluetoothAdapter.isEnabled()) { //request to open bluetooth
+        new MaterialDialog.Builder(this)
+                .title("Please turn bluetooth ON")
+                .content("Bluetooth is required in order to receive signals from your ROV." +
+                        "")
+                .positiveText("OK")
+                .callback(new MaterialDialog.ButtonCallback() {
+                    @Override
+                    public void onPositive(MaterialDialog dialog) {
+                        super.onPositive(dialog);
+                        if (!mBluetoothAdapter.isEnabled())
+                            startActivityForResult(new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE), 1);
+                    }
+                })
+                .show();
+
+    }
 
 
+//        thread.AddMessage("connected");
+}
+    // Start a new Thread to monitor Received bluetooth data
+    public void beginListenForData(){
+    try{
+        Thread.sleep(1000);
+    }catch (InterruptedException e){}
+    try {
+        inStream = thread.getMmSocket().getInputStream();
+    } catch (IOException e){ e.printStackTrace();
+    }
+
+    Thread workerThread = new Thread(new Runnable(){
+        public void run(){
+            while(!Thread.currentThread().isInterrupted() && !stopWorker){
+                try{
+                    int availableCount = inStream.available();
+                    if(availableCount > 0){
+                        char data = (char)inStream.read();
+                        try {
+                            OnBluetoothSerialAvailable(data);
+                        }catch (Exception e){}
+                    }
+                }
+                catch (IOException ex){
+                    stopWorker = true;
+                }
+            }
+        }
+    });
+    workerThread.start();
+}
+    public void OnBluetoothSerialAvailable(Character serialData){
+    //Log.i("BluetoothReceived", serialData.toString());
+    if(currentReadMode==0){
+        if(serialData == SENSOR_HEADER){
+            currentReadMode=1;
+        }
+    }else if(currentReadMode ==1){
+        if(serialData == SENSOR_SERIAL_TYPE){
+            currentReadMode = 2;
+        }
+    }else if(currentReadMode == 2){
+        switch (currentCount){
+            case 1:
+                tempValue += (serialData-48)*1000;
+                currentCount = 2;
+                break;
+            case 2:
+                tempValue += (serialData-48)*100;
+                currentCount = 3;
+                break;
+            case 3:
+                tempValue += (serialData-48)*10;
+                currentCount = 4;
+                break;
+            case 4:
+                tempValue += (serialData-48);
+                currentCount = 1;
+                currentReadMode = 3;
+                break;
+            default:
+                currentCount = 1;
+        }
+    }else if(currentReadMode == 3){
+        sensorValue[serialData-48] = tempValue;
+        tempValue = 0;
+        Log.i("sensor "+(serialData-48)+" Data",Integer.toString(sensorValue[serialData-48]));
+        sensorChart[serialData-48+1].addEntry(sensorValue[serialData-48]);
+        sensorChart[serialData-48+1].lineChart.invalidate();
+
+        currentReadMode = 0;
+    }
+//        Log.i("currentReadMode",Integer.toString(currentReadMode));
+}
+    private void requestEnableBluetooth(){
+    paired = mBluetoothAdapter.getBondedDevices();
+    int deviceCount = 0;
+    if(paired.size() > 0){
+        for(BluetoothDevice device : paired){
+            deviceCount++;
+            itemList.add(/*"  "+deviceCount+". "+*/device.getName());
+        }
+        itemString = new String[itemList.size()];
+        itemList.toArray(itemString);
+    }
+
+    MaterialDialog.Builder dialog = new MaterialDialog.Builder(this);
+    dialog.title("Please Select your ROV");
+    dialog.items(itemString);
+    dialog.itemsCallbackSingleChoice(-1, new MaterialDialog.ListCallbackSingleChoice() {
+        @Override
+        public boolean onSelection(MaterialDialog materialDialog, View view, int which, CharSequence charSequence) {
+            selected = which;
+            Log.i("BD selected",selected+"");
+            Log.i("BluetoothDeviceSelected", itemString[which]);
+            if (paired.size() > 0) {
+                for (BluetoothDevice device : paired) {
+                    if (device.getName().equals(itemString[which])) {
+                        thread = new BluetoothThread(device);
+                        thread.start();
+                        thread.AddMessage("connected");
+                        beginListenForData();
+                        Toast.makeText(SensorMoitor.this,"Connecting to "+itemString[which],Toast.LENGTH_LONG).show();
+                        try{
+                            Thread.sleep(2000);
+                        }catch (InterruptedException e){}
+                        Toast.makeText(SensorMoitor.this,"Connected",Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+
+            return false;
+        }
+    });
+    dialog.positiveText("Connect");
+    dialog.show();
+}
 
 }
